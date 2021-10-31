8f9016476726d7712f09113bf8f4ecd5273fce0d
AndreiSerbanat <serbanat.andrei.20@stud.umfst.ro>
2021-10-31 18:37:45 +0200

    character changes

--- src/Character/character.java
+++ src/Character/character.java
@@ -15,22 +15,12 @@ public class character {
 
     //endregion
 
-    //region Skill INIT
 
-    // Fields: level, xp, RankUpXP | Methods: get, set, add
-    public Skill Harvesting = new Skill(0, 0, 100);
-    public Skill Logging  = new Skill(0, 0, 100);
-    public Skill Mining  = new Skill(0, 0, 100);
-    public Skill Skinning  = new Skill(0, 0, 100);
-    public Skill Fishing  = new Skill(0, 0, 100);
-
-    //endregion
 
     //region Metrics INIT
 
     // Fields: Max, Value | Methods: get, set, add
     public Metrics Health = new Metrics(100, 100);
-    public Metrics Mana = new Metrics(100, 100);
     public Metrics Stamina = new Metrics(100, 100);
 
     //endregion
