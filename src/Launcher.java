e0d47c1323790998f831eaeeb4017cbf535cabde
PandiPatrick <91600100+PandiPatrick@users.noreply.github.com>
2021-11-02 21:03:32 +0200

    Added phase 1 of saves
    Need the character info to be in a more solid state of operation, with proper initialization so I can send over an array of characters when loading a save.

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,10 @@
 import game.Game;
 import game.GameLoop;
+import saves.SaveLoader;
 
 public class Launcher {
-    public static void main(String[] args) { new Thread(new GameLoop(new Game(800, 600))).start();}
+    public static void main(String[] args) {
+        SaveLoader sl = new SaveLoader();
+        new Thread(new GameLoop(new Game(800, 600))).start();
+        }
 }
